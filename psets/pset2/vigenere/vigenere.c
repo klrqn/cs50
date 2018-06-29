1143d140e7cd759439ff72f41d9d2a5542547507
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-06-29 14:35:23 +0000

    vigenere rotate bug

--- psets/pset2/vigenere/vigenere.c
+++ psets/pset2/vigenere/vigenere.c
@@ -43,34 +43,36 @@ int main(int argc, string argv[])
 
 string encipher(string orig_msg, string keyword)
 {
-    printf("original message: %s\nkeyword: %s\n", orig_msg, keyword);
-    // get msg length
+    // printf("ciphertext: ");
 
+    // get msg length
     int msg_len = strlen(orig_msg);
     int key_len = strlen(keyword);
 
     // loop through all characters of the phrase
+    int counter = 0;
     for (int i = 0; i < msg_len; i++)
     {
-
-
         // printf("original message at [%i]: %c\n", i, orig_msg[i]);
-        // printf("keyword  message at [%i]: %c\n", i, (((keyword[i % key_len]-'a')%26)+'a'));
-        // printf("keyword  message at [%i]: %i\n", i, (((keyword[i % key_len]-'a')%26)+'a'));
-        // printf("enciphe  message at [%i]: %c\n", i, (orig_msg[i] + keyword[i % key_len]));
-
+        // printf("keyword  message at [%i]: %c\n", i, (keyword[counter % key_len]-'a'));
+        // printf("keyword  message at [%i]: %i\n", i, (keyword[counter % key_len]-'A'));
+        // printf("enciphe  message at [%i]: %c\n", i, (orig_msg[i] + (keyword[counter % key_len]-'a')));
 
         // if the letter is lower case
         if (orig_msg[i] >= 'a' && orig_msg[i] <= 'z')
         {
+            printf("rotate by: %i\n", (keyword[counter % key_len]-'a'));
             // rotate by the input num within all lowercase chars
-            printf("%c", (((orig_msg[i] + (keyword[i % key_len]-'a') - 'a') % 26) + 'a'));
+            // printf("%c", (((orig_msg[i] + (keyword[counter % key_len]-'a') - 'a') % 26) + 'a'));
+            counter++;
         }
         // else if the letter is lower case
         else if (orig_msg[i] >= 'A' && orig_msg[i] <= 'Z')
         {
             // rotate by the input num within all uppercase chars
-            printf("%c", (((orig_msg[i] + (keyword[i % key_len]-'A') - 'A') % 26) + 'A'));
+            printf("rotate by: %i\n", (keyword[counter % key_len]-'A'));
+            // printf("%c", (((orig_msg[i] + (keyword[counter % key_len]-'A') - 'A') % 26) + 'A'));
+            counter++;
         }
         else
         {
@@ -85,20 +87,9 @@ string encipher(string orig_msg, string keyword)
     return 0;
 }
 
-// :) vigenere.c exists.
-// :) vigenere.c compiles.
-// :( encrypts "a" as "a" using "a" as keyword
-//     expected "ciphertext: a\n", not "original messag..."
-// :( encrypts "barfoo" as "caqgon" using "baz" as keyword
-//     expected "ciphertext: caq...", not "original messag..."
 // :( encrypts "BaRFoo" as "CaQGon" using "BaZ" as keyword
-//     expected "ciphertext: CaQ...", not "original messag..."
-// :( encrypts "BARFOO" as "CAQGON" using "BAZ" as keyword
-//     expected "ciphertext: CAQ...", not "original messag..."
-// :( encrypts "world!$?" as "xoqmd!$?" using "baz" as keyword
-//     expected "ciphertext: xoq...", not "original messag..."
+//    "ciphertext: CaQGoh"
+
+
 // :( encrypts "hello, world!" as "iekmo, vprke!" using "baz" as keyword
-//     expected "ciphertext: iek...", not "original messag..."
-// :) handles lack of argv[1]
-// :) handles argc > 2
-// :) rejects "Hax0r2" as keyword
+//    "ciphertext:  iekmo, wnslc!"
